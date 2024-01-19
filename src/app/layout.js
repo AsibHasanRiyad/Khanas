2877c5dec2818172f1f144063fcdbf2b04bf9852
Asib Hasan Riyad <asibhasanriyad@gmail.com>
2024-01-19 12:19:21 +0600

    footer is added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/Footer/Footer";
+import { NavbarWithMegaMenu } from "@/components/Navbar/NavListMenu";
 
 const open_sans = Open_Sans({ subsets: ["latin"] });
 
@@ -12,7 +14,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={open_sans.className}>
-        <main className=" max-w-[1920px] mx-auto ">{children}</main>
+        <main className=" max-w-[1920px] mx-auto ">
+          <NavbarWithMegaMenu />
+          <div className=" min-h-screen justify-center items-center">
+            {children}
+          </div>
+          <Footer />
+        </main>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import Banner from "@/components/Banner/Banner";
 import Delivery from "@/components/Delivery/Delivery";
 import MenuItems from "@/components/MenuItems/MenuItems";
-import { NavbarWithMegaMenu } from "@/components/Navbar/NavListMenu";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <NavbarWithMegaMenu />
       <Banner />
       <MenuItems />
       <Delivery />
